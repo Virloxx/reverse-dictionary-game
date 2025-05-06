5ec0c6f947f1c7dfb2a11950b2d0f5d98eba5d4e
Oskar Pankowski <oskar.pankowski01@gmail.com>
2025-05-06 22:53:44 +0200

    test commit to check netlify connection

--- src/app/page.js
+++ src/app/page.js
@@ -254,7 +254,7 @@ export default function ReverseDictionaryGame() {
                 >
                   I don’t know
                 </button>
-              </>
+              </> 
             ) : (
               <button
                 onClick={() => {
